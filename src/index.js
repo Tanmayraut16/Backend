14ef9982951d0e055b1260b61660608c7a1b1d1c
Tanmayraut16 <Tanmayraut16@outlook.com>
2024-12-19 15:13:14 +0530

    added util pages

--- src/index.js
+++ src/index.js
@@ -6,4 +6,18 @@ dotenv.config({
     path: "./env"
 });
 
-connectDB();
+
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Error : ", error);
+        throw error
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed!! ", err);
+})
